65de59666402bf9b8a51735b111ed4ed58f9b84a
Darrell Good <darzgood@gmail.com>
2018-11-08 14:59:16 -0500

    relabeled the TeleOP name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOP.java
@@ -76,7 +76,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="NeilTest", group="Iterative Opmode")
+@TeleOp(name="Auto Parent", group="Iterative Opmode")
 @Disabled
 public abstract class AutoOP extends LinearOpMode {
     // Declare OpMode members.
