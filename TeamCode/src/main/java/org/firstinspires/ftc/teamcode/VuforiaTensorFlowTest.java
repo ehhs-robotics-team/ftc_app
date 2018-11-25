f5384d4acc5091a0876f6e2e068de41c278eb49d
Darrell Good <darzgood@gmail.com>
2018-11-25 18:16:21 -0500

    Added tfodScan method to scan for mineral samples

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTensorFlowTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTensorFlowTest.java
@@ -40,6 +40,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.ArrayList;
@@ -147,7 +148,7 @@ public class VuforiaTensorFlowTest extends LinearOpMode {
     private TFObjectDetector tfod;
 
     @Override public void runOpMode() {
-
+        main();
     }
 
     public void main() {
@@ -165,7 +166,10 @@ public class VuforiaTensorFlowTest extends LinearOpMode {
         telemetry.update();
         waitForStart();
 
-        navigationTargetsScan();
+        while(opModeIsActive()) {
+            navigationTargetsScan();
+            tfodScan();
+        }
 
 
     }
@@ -290,20 +294,16 @@ public class VuforiaTensorFlowTest extends LinearOpMode {
         for (VuforiaTrackable trackable : allTrackables) {
             ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
         }
+
+        /** Start tracking the data sets we care about. */
+        targetsRoverRuckus.activate();
     }
 
     /**
      * Scan for all the targets and print output.
      */
     private void navigationTargetsScan() {
-        /** Wait for the game to begin */
-        telemetry.addData(">", "Press Play to start tracking");
-        telemetry.update();
-        waitForStart();
-
-        /** Start tracking the data sets we care about. */
-        targetsRoverRuckus.activate();
-        while (opModeIsActive()) {
+        if (opModeIsActive()) {
 
             // check all the trackable target to see which one (if any) is visible.
             targetVisible = false;
@@ -365,5 +365,57 @@ public class VuforiaTensorFlowTest extends LinearOpMode {
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+    }
+
+    /**
+     * Scan for the minerals and telemetry out which is the gold.
+     * This is for development only.
+     */
+    public void tfodScan() {
+        if (opModeIsActive()) {
+            /** Activate Tensor Flow Object Detection. */
+            if (tfod != null) {
+                tfod.activate();
+            }
+
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        if (updatedRecognitions.size() == 3) {
+                            int goldMineralX = -1;
+                            int silverMineral1X = -1;
+                            int silverMineral2X = -1;
+                            for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    goldMineralX = (int) recognition.getLeft();
+                                } else if (silverMineral1X == -1) {
+                                    silverMineral1X = (int) recognition.getLeft();
+                                } else {
+                                    silverMineral2X = (int) recognition.getLeft();
+                                }
+                            }
+                            if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                                if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                                    telemetry.addData("Gold Mineral Position", "Left");
+                                } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                                    telemetry.addData("Gold Mineral Position", "Right");
+                                } else {
+                                    telemetry.addData("Gold Mineral Position", "Center");
+                                }
+                            }
+                        }
+                        telemetry.update();
+                    }
+                }
+            }
+        }
+
+        if (tfod != null) {
+            tfod.shutdown();
+        }
     }
 }
