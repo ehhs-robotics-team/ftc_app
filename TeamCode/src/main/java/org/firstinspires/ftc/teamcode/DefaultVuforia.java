6ef46f33af1724d527908764cd66d8750482937f
Darrell Good (Nerdz Programming) <darzgood@gmail.com>
2019-04-09 17:28:13 -0400

    renamed Default vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefaultVuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefaultVuforia.java
@@ -49,7 +49,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
+@TeleOp(name = "Default Vuforia", group = "Concept")
 
 public class DefaultVuforia extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
